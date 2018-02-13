10100a1f4a2998fca305e30728db74b438eac0f1
angelsolaorbaiceta <angelsolaorbaiceta@gmail.com>
2018-02-13 23:56:02 +0100

    use conjugate gradient with preconditioner

--- process/structureprocess.go
+++ process/structureprocess.go
@@ -35,12 +35,13 @@ func Solve(s *preprocess.Structure, options SolveOptions) {
 		go mat.ToImage(sysMatrix, options.OutputPath)
 	}
 
-	solver := lineq.ConjugateGradientSolver{MaxError: 1e-5, MaxIter: sysVector.Length()}
+	solver := lineq.PreconditionedConjugateGradientSolver{MaxError: 1e-5, MaxIter: sysVector.Length()}
 	if !solver.CanSolve(sysMatrix, sysVector) {
 		panic("Solver cannot solve system!")
 	}
 	displSolutions := solver.Solve(sysMatrix, sysVector)
 	fmt.Println(displSolutions)
+	fmt.Println(sysMatrix.TimesVector(displSolutions.Solution).Minus(sysVector))
 }
 
 func makeSystemOfEqs(s *preprocess.Structure) (mat.Matrixable, *vec.Vector) {
@@ -81,6 +82,7 @@ func addTermsToStiffnessMatrix(m mat.Matrixable, e *preprocess.Element) {
 		for i := 0; i < stiffMat.Rows(); i++ {
 			for j := 0; j < stiffMat.Cols(); j++ {
 				if stiffVal = stiffMat.Value(i, j); !inkmath.IsCloseToZero(stiffVal) {
+					fmt.Println("i,j = ", i, ",", j, "VAL:", stiffVal, "->", dofs[i], ", ", dofs[j])
 					m.AddToValue(dofs[i], dofs[j], stiffVal)
 				}
 			}
