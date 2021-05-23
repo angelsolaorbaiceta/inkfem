d3b63273fcfcf5d5d886dff954a90da0fbd96abe
Angel <angelsolaorbaiceta@gmail.com>
2021-05-23 10:35:10 +0200

    rename file

---
+++ process/solve_options.go
@@ -0,0 +1,11 @@
+package process
+
+/*
+SolveOptions includes configuration parameters for structural solving process.
+*/
+type SolveOptions struct {
+	SaveSysMatrixImage    bool
+	OutputPath            string
+	SafeChecks            bool
+	MaxDisplacementsError float64
+}
