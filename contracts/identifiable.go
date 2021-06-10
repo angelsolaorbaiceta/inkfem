a5faf03167fbecbde226b58f86b0eb20092b5ba5
angelsolaorbaiceta <angelsolaorbaiceta@gmail.com>
2021-06-10 07:50:10 +0200

    (docs) fix comment

--- contracts/identifiable.go
+++ contracts/identifiable.go
@@ -3,9 +3,7 @@ package contracts
 // StrID is the type used for structural data ids.
 type StrID = string
 
-/*
-Identifiable is anything that can be referenced using an integer number.
-*/
+// Identifiable is anything that can be identified using a string.
 type Identifiable interface {
 	GetID() StrID
 }
