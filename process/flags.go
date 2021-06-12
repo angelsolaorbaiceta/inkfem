63521a1c861a28fb0af1ba61673acfed2082009d
angelsolaorbaiceta <angelsolaorbaiceta@gmail.com>
2021-06-12 11:10:34 +0200

    Include weight flag and document flags in readme

--- process/flags.go
+++ process/flags.go
@@ -7,24 +7,28 @@ import (
 
 // CLIFlags contains the input flags to the CLI program
 type CLIFlags struct {
-	InputFilePath  *string
-	Verbose        *bool
-	Preprocess     *bool
-	SysMatrixToPng *bool
-	SafeChecks     *bool
-	DispMaxError   *float64
+	InputFilePath          *string
+	Verbose                *bool
+	Preprocess             *bool
+	SysMatrixToPng         *bool
+	SafeChecks             *bool
+	DispMaxError           *float64
+	ShouldIncludeOwnWeight *bool
 }
 
-// ParseOrShowUsage reads the program flags and parses them.
-// If the required flags are not passed, shows the usage of the cli.
+/*
+ParseOrShowUsage reads the program flags and parses them.
+If the required flags are not passed, shows the usage of the cli.
+*/
 func ParseOrShowUsage() CLIFlags {
 	flags := CLIFlags{
-		InputFilePath:  flag.String("i", "", "input file path"),
-		Verbose:        flag.Bool("v", false, "verbose?"),
-		Preprocess:     flag.Bool("p", false, "dump preprocessed structure to file?"),
-		SysMatrixToPng: flag.Bool("m", false, "save system of equations matrix to png image file?"),
-		SafeChecks:     flag.Bool("safe", false, "perform safety checks?"),
-		DispMaxError:   flag.Float64("e", 1e-5, "maximum allowed displacements error"),
+		InputFilePath:          flag.String("i", "", "input file path"),
+		Verbose:                flag.Bool("v", false, "verbose?"),
+		Preprocess:             flag.Bool("p", false, "dump preprocessed structure to file?"),
+		SysMatrixToPng:         flag.Bool("mat", false, "save system of equations matrix to png image file?"),
+		SafeChecks:             flag.Bool("safe", false, "perform safety checks?"),
+		DispMaxError:           flag.Float64("error", 1e-5, "maximum allowed displacements error"),
+		ShouldIncludeOwnWeight: flag.Bool("weight", false, "include the elements self weight as distributed load?"),
 	}
 
 	flag.Parse()
