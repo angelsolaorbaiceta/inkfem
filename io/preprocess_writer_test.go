9927ac2ae4d6c87eb660bf6bd368d799a7e9951d
Angel <angelsolaorbaiceta@gmail.com>
2022-03-13 10:31:36 +0100

    Test nodes

--- io/preprocess_writer_test.go
+++ io/preprocess_writer_test.go
@@ -3,6 +3,7 @@ package io
 import (
 	"bytes"
 	"fmt"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -30,22 +31,32 @@ func TestWritePreprocessedStructure(t *testing.T) {
 				WithSection(structure.MakeUnitSection()).
 				WithMaterial(structure.MakeUnitMaterial()).
 				Build()
+		preNodes = []*preprocess.Node{
+			preprocess.MakeNode(nums.MinT, originalElement.StartPoint(), 10, 20, 30),
+			preprocess.MakeNode(nums.HalfT, originalElement.PointAt(nums.HalfT), 11, 21, 31),
+			preprocess.MakeNode(nums.MaxT, originalElement.EndPoint(), 12, 22, 32),
+		}
 		elements = []*preprocess.Element{
-			preprocess.MakeElement(
-				originalElement,
-				[]*preprocess.Node{
-					preprocess.MakeNode(nums.MinT, originalElement.StartPoint(), 10, 20, 30),
-					preprocess.MakeNode(nums.HalfT, originalElement.PointAt(nums.HalfT), 11, 21, 31),
-					preprocess.MakeNode(nums.MaxT, originalElement.EndPoint(), 12, 22, 32),
-				},
-			),
+			preprocess.MakeElement(originalElement, preNodes),
 		}
 		str    = preprocess.MakeStructure(metadata, nodesById, elements)
 		writer bytes.Buffer
 	)
 
+	// Add left load to first node
+	preNodes[0].AddLocalLeftLoad(5, 10, 15)
+
+	// Add right load to last node
+	preNodes[2].AddLocalRightLoad(-5, -10, -15)
+
 	WritePreprocessedStructure(str, &writer)
-	gotLines := strings.Split(writer.String(), "\n")
+
+	var gotLines []string
+	for _, line := range strings.Split(writer.String(), "\n") {
+		if line != "" {
+			gotLines = append(gotLines, line)
+		}
+	}
 
 	fmt.Println(writer.String())
 
@@ -59,8 +70,30 @@ func TestWritePreprocessedStructure(t *testing.T) {
 	t.Run("then goes the degrees of freedom count", func(t *testing.T) {
 		// 3 nodes x 3dof = 9 total dofs
 		want := "dof_count: 9"
-		if got := gotLines[2]; got != want {
+		if got := gotLines[1]; got != want {
 			t.Errorf("Want '%s', got '%s'", want, got)
+		}
+	})
+
+	t.Run("then go the original nodes", func(t *testing.T) {
+		var (
+			wantHeader         = "|nodes| 2"
+			wantNodeOnePattern = "n1 -> 0(\\.[0]+)? 0(\\.[0]+)? { } | DOF: \\[6 7 8\\]"
+			wantNodeTwoPattern = "n2 -> 200(\\.[0]+)? 0(\\.[0]+)? { dx dy rz } | DOF: \\[0 1 2\\]"
+		)
+
+		if got := gotLines[2]; got != wantHeader {
+			t.Errorf("Want '%s', got '%s'", wantHeader, got)
+		}
+
+		// Order in which the nodes appear isn't guaranteed
+		nodeLines := gotLines[3] + " " + gotLines[4]
+
+		if match, _ := regexp.MatchString(wantNodeOnePattern, nodeLines); !match {
+			t.Error("Want node one")
+		}
+		if match, _ := regexp.MatchString(wantNodeTwoPattern, nodeLines); !match {
+			t.Error("Want node two")
 		}
 	})
 }
