cfa278cafeb11b90550086bfda3bb4aad87a31ce
angel <angelsolaorbaiceta@gmail.com>
2022-03-26 20:13:39 +0100

    Basic generate command implementation

--- cmd/generate.go
+++ cmd/generate.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/angelsolaorbaiceta/inkfem/generate"
+	iodef "github.com/angelsolaorbaiceta/inkfem/io/def"
+	"github.com/angelsolaorbaiceta/inkfem/structure"
 	"github.com/spf13/cobra"
 )
 
@@ -13,13 +16,16 @@ const (
 )
 
 var (
-	generateType  string
-	generateSpans int
+	generateType        string
+	generateSpans       int
+	generateSpanLength  float64
+	generateLevels      int
+	generateLevelHeight float64
 
 	generateCommand = &cobra.Command{
 		Use:   "generate --type=<type>",
 		Short: "Generates a structure",
-		Long:  "Generates a structure of the given type.",
+		Long:  "Generates a structure with the given typology.",
 		Run:   generateStructure,
 	}
 )
@@ -34,22 +40,45 @@ func init() {
 		Flags().
 		IntVarP(&generateSpans, "spans", "s", 10, "the number of horizontal spans")
 
+	generateCommand.
+		Flags().
+		Float64VarP(&generateSpanLength, "span", "p", 400.0, "the length of each span")
+
+	generateCommand.
+		Flags().
+		IntVarP(&generateLevels, "levels", "l", 5, "the number of vertical levels")
+
+	generateCommand.
+		Flags().
+		Float64VarP(&generateLevelHeight, "level", "e", 300.0, "the height of each level")
+
 	rootCmd.AddCommand(generateCommand)
 }
 
 func generateStructure(cmd *cobra.Command, args []string) {
 	switch generateType {
-	case reticType:
-	case reticLongType:
+	case reticType, reticLongType:
 		{
-			fmt.Printf("generating %d spans\n", generateSpans)
-
+			str := generate.Reticular(generate.ReticStructureParams{
+				Spans:         generateSpans,
+				Span:          generateSpanLength,
+				Levels:        generateLevels,
+				Height:        generateLevelHeight,
+				LoadDistValue: 50.0,
+				Section:       structure.MakeSection("sec", 1.0, 1.0, 1.0, 1, 0),
+				Material:      structure.MakeMaterial("mat", 1.0, 1.0, 1.0, 1.0, 1.0, 1.0),
+			})
+			iodef.Write(str, os.Stdout)
 		}
 	default:
 		{
-			fmt.Printf("Unknown structure typology: \"%s\"\n", generateType)
-			fmt.Println("Please use one of the following:")
-			fmt.Printf("\t- \"%s\" or \"%s\": A reticular frame made of beams and columns\n", reticType, reticLongType)
+			fmt.Printf("Unknown structure typology: \"%s\".\n", generateType)
+			fmt.Println("Use one of the following:")
+			fmt.Printf(
+				"\t- \"%s\" or \"%s\": A reticular frame made of beams and columns with \"spans\" beams per level and \"levels\" columns.\n",
+				reticType,
+				reticLongType,
+			)
 
 			os.Exit(1)
 		}
