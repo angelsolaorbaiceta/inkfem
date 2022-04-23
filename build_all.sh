ad217912a50d9ba9df241ce4fd2bd41331fc8a3f
angel <angelsolaorbaiceta@gmail.com>
2022-04-23 10:15:21 +0200

    Improve the build all script using a function

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,24 @@
 # Builds the CLI for all operating systems
-GOOS=linux GOARCH=amd64 go build -ldflags "-s -w" -o bin/inkfem_linux_amd64 inkfem.go
-GOOS=linux GOARCH=arm64 go build -ldflags "-s -w" -o bin/inkfem_linux_arm64 inkfem.go
+rm -rf bin/
 
-GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w" -o bin/inkfem_darwin_amd64 inkfem.go
-GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w" -o bin/inkfem_darwin_arm64 inkfem.go
+build_for_os() {
+  echo Building for $1 OS, with $2 arch
+  
+  FILE_PATH=bin/inkfem_$1_$2
+  GOOS=$1 GOARCH=$2 go build -ldflags "-s -w" -o $FILE_PATH inkfem.go
+  shasum -a 256 $FILE_PATH >> bin/sha256sums.txt
+  gzip -9 $FILE_PATH
+}
 
-GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o bin/inkfem_windows_amd64.exe inkfem.go
+# Linux
+build_for_os linux amd64
+build_for_os linux arm64
+
+# OSX
+build_for_os darwin amd64
+build_for_os darwin arm64
+
+# Windows
+build_for_os windows amd64
+
+echo Done!
