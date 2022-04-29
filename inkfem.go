106761ab0a2e2d34d1e65436ca6142eaa800bc99
Angel <angelsolaorbaiceta@gmail.com>
2022-04-29 09:11:55 +0200

    Read version from the VERSION file

--- inkfem.go
+++ inkfem.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	_ "embed"
+
 	"github.com/angelsolaorbaiceta/inkfem/cmd"
+	inkio "github.com/angelsolaorbaiceta/inkfem/io"
 )
 
+//go:embed VERSION
+var versionString string
+
 func main() {
+	inkio.SetBinaryVersion(versionString)
 	cmd.Execute()
 }
--- io/read_version.go
+++ io/read_version.go
@@ -1,6 +1,7 @@
 package io
 
 import (
+	"fmt"
 	"regexp"
 	"strconv"
 
@@ -8,20 +9,40 @@ import (
 )
 
 var versionRegex = regexp.MustCompile(`(?:inkfem\s+v)(\d+)(?:[.])(\d+)`)
+var binaryMajorVersion int = -1
+var binaryMinorVersion int = -1
 
-// parseVersionNumbers expectes the passed in first line to follow the format "inkfem vM.m"
-// where "M" and "m" are the major and minor versions of the application that created
-// the parsed file. It returns these two version numbers or panics if the line couldn't
-// be matched.
-func ParseVersionNumbers(firstLine string) (majorVersion, minorVersion int) {
-	if foundMatch := versionRegex.MatchString(firstLine); !foundMatch {
+// SetBinaryVersion saves the binary version numbers.
+// This function should be called when the binary is executed to make the version numbers available
+// to the rest of the application.
+func SetBinaryVersion(verstionString string) {
+	majorVersion, minorVersion := ParseVersionNumbers(verstionString)
+	binaryMajorVersion = majorVersion
+	binaryMinorVersion = minorVersion
+}
+
+// GetBinaryVersion returns the binary version numbers.
+// This numbers should have been set by the SetBinaryVersion function. Panics if the version
+// numbers weren't set.
+func GetBinaryVersion() (majorVersion, minorVersion int) {
+	if binaryMajorVersion == -1 || binaryMinorVersion == -1 {
+		panic("inkfem binary version not set")
+	}
+
+	return binaryMajorVersion, binaryMinorVersion
+}
+
+// ParseVersionNumbers expectes the passed in string to follow the format "inkfem vM.m"
+// where "M" and "m" are the major and minor versions of the application.
+// It returns these two version numbers or panics if the line couldn't be matched.
+func ParseVersionNumbers(versionString string) (majorVersion, minorVersion int) {
+	if foundMatch := versionRegex.MatchString(versionString); !foundMatch {
 		panic(
-			"Could not parse major and minor version numbers." +
-				"Are you missing 'inkfem vM.m' in your file's first line?",
+			fmt.Sprintf("Could not parse version string '%s' (expected inkfem vM.m)", versionString),
 		)
 	}
 
-	versions := versionRegex.FindStringSubmatch(firstLine)
+	versions := versionRegex.FindStringSubmatch(versionString)
 	majorVersion, _ = strconv.Atoi(versions[1])
 	minorVersion, _ = strconv.Atoi(versions[2])
 
