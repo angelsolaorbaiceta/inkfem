d0b66c455d55abca907b577297bab718d4161d1c
angel <angelsolaorbaiceta@gmail.com>
2024-08-18 10:29:31 +0200

    Polygon of an Fx load

--- plot/dist_loads.go
+++ plot/dist_loads.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	// distLoadLinePositions are the positions where the distributed load lines
-	// with the arrowheads are drawn.
+	// with the arrowheads are drawn. We draw them at 10% intervals of the length.
 	distLoadLinePositions = []nums.TParam{
 		nums.MakeTParam(0.1),
 		nums.MakeTParam(0.2),
@@ -51,21 +51,70 @@ func drawDistributedLoad(
 	ctx *plotContext,
 ) {
 	if dLoad.IsInLocalCoords {
-		drawLocalDistributedLoad(dLoad, bar, ctx)
+		switch dLoad.Term {
+		case load.FX:
+			drawLocalDistributedFxLoad(dLoad, bar, ctx)
+		case load.FY:
+			drawLocalDistributedFyLoad(dLoad, bar, ctx)
+		}
 	} else {
 		// TODO: draw distributed load in global coords
 	}
 }
 
-// The load is represented as a polygon with the following vertices:
+// The Fx load is represented as a polygon with the following vertices:
 //
 //  1. The start node, which is at (0, 0) due to the transformation.
-//  2. With x = 0, move y by the negative value of the load's start value.
-func drawLocalDistributedLoad(
+//  2. With x = 0, move y by the value of the load's start value.
+//  3. With x = endX, move y by the value of the load's end value.
+//  4. The end node, which is at (endX, 0) due to the transformation.
+//
+// By convention, the value of the Fx load is drawn in the Y axis, with the arrows
+// pointing in the X axis direction, according to the load's sign.
+func drawLocalDistributedFxLoad(
 	dLoad *load.DistributedLoad,
 	bar *structure.Element,
 	ctx *plotContext,
 ) {
+	if dLoad.Term != load.FX {
+		panic(fmt.Sprintf("Invalid distributed load term: %s. Expected Fx", dLoad.Term))
+	}
+
+	var (
+		canvas    = ctx.canvas
+		scale     = ctx.unitsScale
+		loadScale = ctx.options.DistLoadScale
+
+		endX   = int(scale.applyToLength(bar.Length()))
+		startY = int(dLoad.StartValue * loadScale)
+		endY   = int(dLoad.EndValue * loadScale)
+
+		x = []int{0, 0, endX, endX}
+		y = []int{0, startY, endY, 0}
+	)
+
+	canvas.Polygon(x, y)
+}
+
+// The Fy load is represented as a polygon with the following vertices:
+//
+//  1. The start node, which is at (0, 0) due to the transformation.
+//  2. With x = 0, move y by the negative value of the load's start value.
+//  3. With x = endX, move y by the negative value of the load's end value.
+//  4. The end node, which is at (endX, 0) due to the transformation.
+//
+// By convention, Fy loads are drawn with the arrows pointing towards the bar.
+// Thus, the polygon is drawn in the opposite direction of the load, for which
+// we need to invert the sign of the load values.
+func drawLocalDistributedFyLoad(
+	dLoad *load.DistributedLoad,
+	bar *structure.Element,
+	ctx *plotContext,
+) {
+	if dLoad.Term != load.FY {
+		panic(fmt.Sprintf("Invalid distributed load term: %s. Expected Fy", dLoad.Term))
+	}
+
 	var (
 		canvas    = ctx.canvas
 		scale     = ctx.unitsScale
