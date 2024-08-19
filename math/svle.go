89a501b4f6818ef45fe9309f6f6cceb20bb0565d
Angel <angel.sola@glovoapp.com>
2024-08-19 12:34:01 +0200

    Single variable linear equation

---
+++ math/svle.go
@@ -0,0 +1,60 @@
+package math
+
+import (
+	"fmt"
+
+	"github.com/angelsolaorbaiceta/inkmath/nums"
+)
+
+// SingleVarLinEq represents a single variable linear equation of the form:
+//
+//	y = a*x + b
+type SingleVarLinEq struct {
+	a, b         float64
+	isHorizontal bool
+}
+
+func (svle SingleVarLinEq) YIntercept() float64 {
+	return svle.b
+}
+
+func (svle SingleVarLinEq) Slope() float64 {
+	return svle.a
+}
+
+// MakeSVLE creates a new single variable linear equation from the given coefficients.
+func MakeSVLE(a, b float64) SingleVarLinEq {
+	isHorizontal := nums.FloatsEqual(a, 0.0)
+	return SingleVarLinEq{a, b, isHorizontal}
+}
+
+// MakeSVLEFromPoints creates a new single value linear equation from two points.
+// The two points must have different x values, otherwise an error is returned.
+func MakeSVLEFromPoints(x1, y1, x2, y2 float64) (*SingleVarLinEq, error) {
+	if nums.FloatsEqual(x1, x2) {
+		return nil, fmt.Errorf("x1 and x2 must be different")
+	}
+
+	var (
+		a            = (y2 - y1) / (x2 - x1)
+		b            = y1 - a*x1
+		isHorizontal = nums.FloatsEqual(a, 0.0)
+	)
+
+	return &SingleVarLinEq{a, b, isHorizontal}, nil
+}
+
+// YAt returns the value of the single variable at a given x value.
+func (svle SingleVarLinEq) YAt(x float64) float64 {
+	return svle.a*x + svle.b
+}
+
+// XAt returns the value of the single variable at a given y value.
+// When the slope is zero (horizontal line), the function returns an error.
+func (svle SingleVarLinEq) XAt(y float64) (float64, error) {
+	if svle.isHorizontal {
+		return 0.0, fmt.Errorf("slope is zero")
+	}
+
+	return (y - svle.b) / svle.a, nil
+}
---
+++ math/svle_test.go
@@ -0,0 +1,52 @@
+package math
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMakeSVLE(t *testing.T) {
+	t.Run("create a new single value linear equation from the origin and another point", func(t *testing.T) {
+		svle, err := MakeSVLEFromPoints(0, 0, 1, 1)
+
+		assert.Nil(t, err)
+		assert.Equal(t, 1.0, svle.Slope())
+		assert.Equal(t, 0.0, svle.YIntercept())
+	})
+
+	t.Run("create a new single value linear equation from two points", func(t *testing.T) {
+		svle, err := MakeSVLEFromPoints(1, 1, 2, 3)
+
+		assert.Nil(t, err)
+		assert.Equal(t, 2.0, svle.Slope())
+		assert.Equal(t, -1.0, svle.YIntercept())
+	})
+
+	t.Run("return an error if the two points have the same x value", func(t *testing.T) {
+		_, err := MakeSVLEFromPoints(1, 1, 1, 3)
+
+		assert.NotNil(t, err)
+	})
+
+	t.Run("find Y value for a given X value", func(t *testing.T) {
+		svle := MakeSVLE(2, 3)
+
+		assert.Equal(t, 5.0, svle.YAt(1))
+	})
+
+	t.Run("find X value for a given Y value", func(t *testing.T) {
+		svle := MakeSVLE(2, 3)
+		x, err := svle.XAt(5)
+
+		assert.Nil(t, err)
+		assert.Equal(t, 1.0, x)
+	})
+
+	t.Run("return an error when trying to find X value for a horizontal line", func(t *testing.T) {
+		svle := MakeSVLE(0, 3)
+		_, err := svle.XAt(5)
+
+		assert.NotNil(t, err)
+	})
+}
