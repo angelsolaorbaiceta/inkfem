4078e45a222f404f0b6f00045408adee081ec700
Angel <angel.sola@glovoapp.com>
2024-08-21 14:41:31 +0200

    Fx decreasing test

--- plot/fx_test.go
+++ plot/fx_test.go
@@ -40,8 +40,7 @@ func TestDrawLocalDistributedFxLoad(t *testing.T) {
 			context = makeContext(&writer, 1.0)
 			startT  = nums.MakeTParam(0.2)
 			endT    = nums.MakeTParam(0.8)
-			value   = 200.0
-			dLoad   = load.MakeDistributed(load.FX, true, startT, value, endT, value)
+			dLoad   = load.MakeDistributed(load.FX, true, startT, 200, endT, 200)
 
 			wantPolygon   = "<polygon points=\"20,0 20,200 80,200 80,0\" />"
 			wantArrowYPos = []int{40, 80, 120, 160}
@@ -62,7 +61,41 @@ func TestDrawLocalDistributedFxLoad(t *testing.T) {
 			gotPoly   = gotLines[0]
 			gotArrows = gotLines[3 : len(wantArrows)+3]
 		)
+
+		assert.Equal(t, wantPolygon, gotPoly)
+		assert.Equal(t, wantArrows, gotArrows)
+	})
+
+	t.Run("Decreasing Fx positive load, full length", func(t *testing.T) {
+		var (
+			writer  bytes.Buffer
+			context = makeContext(&writer, 1.0)
+			startT  = nums.MinT
+			endT    = nums.MaxT
+			dLoad   = load.MakeDistributed(load.FX, true, startT, 200, endT, 120)
+
+			wantPolygon      = "<polygon points=\"0,0 0,200 100,120 100,0\" />"
+			wantArrowYPos    = []int{40, 80, 120, 160}
+			wantArrowXEndPos = []int{100, 100, 100, 50}
+			wantArrows       = make([]string, len(wantArrowYPos))
+		)
+
+		for i, y := range wantArrowYPos {
+			x := wantArrowXEndPos[i]
+			wantArrows[i] = fmt.Sprintf(
+				"<line x1=\"0\" y1=\"%d\" x2=\"%d\" y2=\"%d\" marker-end=\"url(#%s)\" stroke=\"%s\" />",
+				y, x, y, arrowId, stroke,
+			)
+		}
+
+		drawLocalDistributedFxLoad(dLoad, barGeometry, context)
 		fmt.Println(writer.String())
+
+		var (
+			gotLines  = strings.Split(writer.String(), "\n")
+			gotPoly   = gotLines[0]
+			gotArrows = gotLines[3 : len(wantArrows)+3]
+		)
 
 		assert.Equal(t, wantPolygon, gotPoly)
 		assert.Equal(t, wantArrows, gotArrows)
